09638ab64a30ba858e8b46674ea7eefa28a793bf
jaejunlee11 <72538151+jaejunlee11@users.noreply.github.com>
2024-01-11 21:26:13 +0900

    [Bronze V] Title: N 찍기, Time: 996 ms, Memory: 29640 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0;i<N;i++) {
+			System.out.println(i+1);
+		}
+	}
+
+}
