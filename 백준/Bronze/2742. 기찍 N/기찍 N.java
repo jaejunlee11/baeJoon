e25c2c6d17f573a21d74536e83dba0def2e8ee83
jaejunlee11 <72538151+jaejunlee11@users.noreply.github.com>
2024-01-11 21:28:32 +0900

    [Bronze IV] Title: 기찍 N, Time: 1040 ms, Memory: 31512 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = N;i>0;i--) {
+			System.out.println(i);
+		}
+	}
+
+}
