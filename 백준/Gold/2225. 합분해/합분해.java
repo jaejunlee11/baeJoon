3992fa747b5459dbd9ce967c79a2f145c639796e
jaejunlee11 <72538151+jaejunlee11@users.noreply.github.com>
2024-02-01 10:31:40 +0900

    [Gold V] Title: 합분해, Time: 112 ms, Memory: 13112 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -3,7 +3,7 @@
  * 1. N ,K입력 -> N은 1~200, K는 1~200
  * 2. 0~N을 K개 사용해서 N만들기
  * 
- * 풀이 -> 공(구분이 없는) N개가 있고 이를 K개의 방에 넣는 느낌 -> 중복 조합 -> N+K-1 C K-1
+ * 풀이 -> dp로 풀어보기
  * 1. N,K입력 받기
  * 2. arr[N+1][K+1]배열 만들기
  * 3. N = 1, K=x일때 => x
@@ -12,11 +12,19 @@
  * 6. N = 3, k=2일때 => 03 30 21 12 =>4 (3,1) + (2,2)
  * 7. N = 4, k=2일때 => 04 40 13 31 22=>5 (4,1) + (3,2) ??
  * 8. N = 2, k=3일때 => 002 020 200 110 011 101 =>6 (2,2) + (1,3)
- * 9. N = x, k=y일때 => (x,y-1) + (x-1,y) =>조합인듯?
+ * 9. N = x, k=y일때 => (x,y-1) + (x-1,y)
+ * 
+ * 진짜 풀이
+ * 1. N,K입력 받기
+ * 2. arr[N+1][K+1]배열 만들기 ->long타입
+ * 3. N=1 ,K=x인 것 => x로 채우기
+ * 4. N=x , K=1인 것 => 1로 채우기
+ * 5. 배열을 순회하면서 arr[x][y] = arr[x][y-1] + arr[x-1][y] ->이때 나누기 연산을 해서 넣어주기
+ * 6. arr[N][K]출력
  * 
  * 고려해야할 것
- * 1. 공식으로 연산을 때려버리면 팩토리얼 때문에 메모리 초과가 남
- * 2. 팩토리얼 공식이 아니라 중단 점을 미리 만들어 줘야할 듯
+ * 1. 메모리 초과 40000개 배열 -> 160000byte -> 널널함
+ * 2. 타입 -> 10억에서 나눠주고 있으니 long타입 사용하면 될 듯?
  * 
  */
 import java.io.*;
@@ -28,20 +36,18 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int K = sc.nextInt();
-		BigInteger answer = upfactorial(BigInteger.valueOf(N+K-1),K-1).divide(factorial(BigInteger.valueOf(N)));
-		System.out.println(answer.remainder(BigInteger.valueOf(1000000000)));
-	}
-	static BigInteger factorial(BigInteger a) {
-		if(a.compareTo(BigInteger.ONE)==0) {
-			return BigInteger.ONE;
+		long[][] arr = new long[N+1][K+1];
+		for(int i = 1;i<N+1;i++) {
+			arr[i][1] =1;
 		}
-		return a.multiply(factorial(a.subtract(BigInteger.ONE)));
-	}
-	
-	static BigInteger upfactorial(BigInteger a, int k) {
-		if(a.compareTo(BigInteger.valueOf(k))==0) {
-			return BigInteger.ONE;
+		for(int i = 1;i<K+1;i++) {
+			arr[1][i] =i;
 		}
-		return a.multiply(upfactorial(a.subtract(BigInteger.ONE),k));
+		for(int i = 2;i<N+1;i++) {
+			for(int j = 2;j<K+1;j++) {
+				arr[i][j] = (arr[i][j-1] + arr[i-1][j])%1000000000;
+			}
+		}
+		System.out.println(arr[N][K]);
 	}
 }
