d75568367a09a2a9b499c3739c9e3ebb2a16c415
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-02-12 22:52:34 +0900

    [Gold V] Title: 합분해, Time: 80 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,53 +1,31 @@
 /*
  * 문제
- * 1. N ,K입력 -> N은 1~200, K는 1~200
- * 2. 0~N을 K개 사용해서 N만들기
+ * 1. 0부터 N까지의 수를 더해서 N이 되오록 만들기
+ * 2. 덧셈의 숫자가 변해도 다른 것
  * 
- * 풀이 -> dp로 풀어보기
- * 1. N,K입력 받기
- * 2. arr[N+1][K+1]배열 만들기
- * 3. N = 1, K=x일때 => x
- * 4. N = x, k=1일때 => 1 
- * 5. N = 2, k=2일때 => 02 20 11 =>3 (1,1) + (1,2) 
- * 6. N = 3, k=2일때 => 03 30 21 12 =>4 (3,1) + (2,2)
- * 7. N = 4, k=2일때 => 04 40 13 31 22=>5 (4,1) + (3,2) ??
- * 8. N = 2, k=3일때 => 002 020 200 110 011 101 =>6 (2,2) + (1,3)
- * 9. N = x, k=y일때 => (x,y-1) + (x-1,y)
- * 
- * 진짜 풀이
- * 1. N,K입력 받기
- * 2. arr[N+1][K+1]배열 만들기 ->long타입
- * 3. N=1 ,K=x인 것 => x로 채우기
- * 4. N=x , K=1인 것 => 1로 채우기
- * 5. 배열을 순회하면서 arr[x][y] = arr[x][y-1] + arr[x-1][y] ->이때 나누기 연산을 해서 넣어주기
- * 6. arr[N][K]출력
- * 
- * 고려해야할 것
- * 1. 메모리 초과 40000개 배열 -> 160000byte -> 널널함
- * 2. 타입 -> 10억에서 나눠주고 있으니 long타입 사용하면 될 듯?
- * 
+ * 풀이
+ * 1. K개의 상자에 N개의 공을 담는 것
+ * 2. N+K-1CN
+ * 3. dp[][]
  */
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
-
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int K = sc.nextInt();
-		long[][] arr = new long[N+1][K+1];
-		for(int i = 1;i<N+1;i++) {
-			arr[i][1] =1;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		long[][] dp = new long[N+K][N+K];
+		for(int i = 0;i<N+K;i++) {
+			dp[i][0] = 1;
+			dp[i][i] = 1;
 		}
-		for(int i = 1;i<K+1;i++) {
-			arr[1][i] =i;
-		}
-		for(int i = 2;i<N+1;i++) {
-			for(int j = 2;j<K+1;j++) {
-				arr[i][j] = (arr[i][j-1] + arr[i-1][j])%1000000000;
+		for(int i = 1;i<N+K;i++) {
+			for(int j = 1;j<N+K;j++) {
+				dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%1000000000;
 			}
 		}
-		System.out.println(arr[N][K]);
+		System.out.println(dp[N+K-1][K-1]%1000000000);
 	}
 }
