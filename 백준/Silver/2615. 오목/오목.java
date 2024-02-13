d850b5c13fae515061f7b4a8dab47eb58924c000
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-02-13 11:49:06 +0900

    [Silver I] Title: 오목, Time: 84 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -1,162 +1,61 @@
+/*
+ * 문제
+ * 1. 바둑알이 주어짐
+ * 2. 5개를 이으면 승리
+ * 3. 6개 되면 안됨
+ * 4. 가장 왼쪽에 있거나 가장 위쪽에 있는 돌 위치와 승리한 사람 출력
+ * 5. 승자가 없으면 0출력
+ * 
+ * 풀이
+ * 1. 오목판 만들기 -> arr[19][19]
+ * 2. 오목판 입력 받기
+ * 3. 4방 탐새을 위한 dir 생성 -> {{1,0},{0,-1},{1,1},{-1,1}}
+ * 4. 모든 오목판을 탐색하며 돌을 만나는 경우 오목 판단
+ * 	4.1. dir역방향으로 동일한 것이 있는지 탐색있으면 종료
+ * 	4.2. dir 정방향으로 5개가 있는지 탐색 => 6개면 실패
+ * 	4.3. 오목이 있으면 출력 후 종료
+ * 5. 모든 판 탐색 이후에도 없으면 0출력
+ */
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	static int[][] dir = {{1,0},{0,1},{1,1},{-1,1}};
 	public static void main(String[] args) throws Exception {
-		// System.setIn(new FileInputStream("Test5.txt"));
-		// //8방탐색을 위한 dir
-		// int[][] dir = {{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{1,1},{-1,-1}};
-		// 대각 탐색을 위한 dir
-
-		int[][] dir = { { 1, -1 }, { -1, 1 }, { 1, 1 }, { -1, -1 }, { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+		int[][] arr = new int[19][19];
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 바둑판 생성
-		int[][] board = new int[19][19];
-		// 바둑판 채우기
-		for (int i = 0; i < 19; i++) {
+		for(int i = 0;i<19;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 19; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+			for(int j = 0 ;j<19;j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		// 우탐색 -> 5개인 경우 돌의 색과 첫 위치를 출력하고 break;
-		// 돌 위치
-		int fr = 0;
-		int fc = 0;
-		// 돌 색
-		int rockColor = 0;
-		// 연속 값
-		int rockCount = 0;
-		// 가로 탐색
-		A: for (int i = 0; i < 19; i++) {
-			for (int j = 0; j < 19; j++) {
-				if (board[i][j] == 0) {
-//					//16번째 칸 부터는 5개가 못 만들어짐(만들어질 수 있어도 이미 체크한 곳) -> 다음 줄로 이동
-//					if(j>14) continue A;
-					continue;
-				} else {
-					// 우하 방향 대각 확인 -> 좌상도 확인하여 count를 더 해줌
-					fr = i;
-					fc = j;
-					rockColor = board[i][j];
-					rockCount = 1;
-					int nr = 0;
-					int nc = 0;
-					for (int k = 4; k < 6; k++) {
-						nr = fr + dir[k][0];
-						nc = fc + dir[k][1];
-						while (nr >= 0 && nr < 19 && nc >= 0 && nc < 19 && (board[nr][nc] == rockColor)) {
-							rockCount++;
-							nr += dir[k][0];
-							nc += dir[k][1];
-						}
-					}
-					if (rockCount == 5) {
-						System.out.println(rockColor);
-						System.out.println((fr + 1) + " " + (fc + 1));
-						return;
-					}
-				}
-			}
-		}
-		// 세로 탐색
-		A: for (int i = 0; i < 19; i++) {
-			for (int j = 0; j < 19; j++) {
-				if (board[i][j] == 0) {
-//					//16번째 칸 부터는 5개가 못 만들어짐(만들어질 수 있어도 이미 체크한 곳) -> 다음 줄로 이동
-//					if(i>14) continue A;
-					continue;
-				} else {
-					// 우하 방향 대각 확인 -> 좌상도 확인하여 count를 더 해줌
-					fr = i;
-					fc = j;
-					rockColor = board[i][j];
-					rockCount = 1;
-					int nr = 0;
-					int nc = 0;
-					for (int k = 6; k < 8; k++) {
-						nr = fr + dir[k][0];
-						nc = fc + dir[k][1];
-						while (nr >= 0 && nr < 19 && nc >= 0 && nc < 19 && (board[nr][nc] == rockColor)) {
-							rockCount++;
-							nr += dir[k][0];
-							nc += dir[k][1];
-						}
-					}
-					if (rockCount == 5) {
-						System.out.println(rockColor);
-						System.out.println((fr + 1) + " " + (fc + 1));
-						return;
-					}
-				}
-			}
-		}
-		// 대각 탐색(하우)
-		A: for (int i = 0; i < 19; i++) {
-			for (int j = 0; j < 19; j++) {
-				if (board[i][j] == 0) {
-					// 16번째 칸 부터는 5개가 못 만들어짐(만들어질 수 있어도 이미 체크한 곳) -> 다음 줄로 이동
-//					if(j>14) continue A;
-//					if(i>14) continue A;
-					continue;
-				} else {
-					// 우하 방향 대각 확인 -> 좌상도 확인하여 count를 더 해줌
-					fr = i;
-					fc = j;
-					rockColor = board[i][j];
-					rockCount = 1;
-					int nr = 0;
-					int nc = 0;
-					for (int k = 0; k < 2; k++) {
-						nr = fr + dir[k][0];
-						nc = fc + dir[k][1];
-						while (nr >= 0 && nr < 19 && nc >= 0 && nc < 19 && (board[nr][nc] == rockColor)) {
-							rockCount++;
-							nr += dir[k][0];
-							nc += dir[k][1];
-						}
-					}
-					if (rockCount == 5) {
-						System.out.println(rockColor);
-						System.out.println((fr + 5) + " " + (fc - 3));
-						return;
-					}
-				}
-			}
-		}
-		// 대각 탐색(상우)
-		A: for (int i = 0; i < 19; i++) {
-			for (int j = 0; j < 19; j++) {
-				if (board[i][j] == 0) {
-					// 16번째 칸 부터는 5개가 못 만들어짐(만들어 질 수 있어도 그전에 이미 체크한 곳) -> 다음 줄로 이동
-//					if(j>14) continue A;
-					continue;
-				} else {
-					// 우하 방향 대각 확인 -> 좌상도 확인하여 count를 더 해줌
-					fr = i;
-					fc = j;
-					rockColor = board[i][j];
-					rockCount = 1;
-					int nr = 0;
-					int nc = 0;
-					for (int k = 2; k < 4; k++) {
+		for(int i = 0 ;i<19;i++) {
+			for(int j = 0 ;j<19;j++) {
+				if(arr[i][j]!=0) {
+					int stone = arr[i][j];
+					for(int k = 0 ;k<4;k++) {
+						int nr = i - dir[k][0];
+						int nc = j - dir[k][1];
+						if(nr>=0 && nc >=0 && nr<19 && nc<19 && arr[nr][nc]==stone) continue;
 						nr = i + dir[k][0];
 						nc = j + dir[k][1];
-						while (nr >= 0 && nr < 19 && nc >= 0 && nc < 19 && board[nr][nc] == rockColor) {
-							rockCount++;
-							nr += dir[k][0];
-							nc += dir[k][1];
+						int count = 1;
+						for(int l = 0; l<5;l++) {
+							if(nr < 0 || nc <0 || nr>=19 || nc>=19 || arr[nr][nc] != stone) break;
+							count++;
+							nr = nr + dir[k][0];
+							nc = nc + dir[k][1];
 						}
-					}
-					if (rockCount == 5) {
-						System.out.println(rockColor);
-						System.out.println((fr + 1) + " " + (fc + 1));
-						return;
+						if(count == 5) {
+							System.out.println(stone);
+							System.out.println((i+1) + " " + (j+1));
+							return;
+						}
 					}
 				}
 			}
 		}
-		System.out.println("0");
+		System.out.println(0);
 	}
 }
