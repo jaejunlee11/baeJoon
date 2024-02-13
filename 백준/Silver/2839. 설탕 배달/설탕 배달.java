228da7b0eb70f5191416db1fa153732304cb7fc8
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-02-13 13:22:27 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 12884 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,29 +1,35 @@
 /*
- * 1. 5로 나눠서 5kg 포대 갯수 구하기 -> 이때 4,7일 때 문제 발생
- * 2. 나머지 계산
- * 	2.1. 나머지가 1인 경우 -> 포대 1개 추가(1kg -> 6kg 포대 1개 감소 -> 3kg포대 2개)
- * 	2.2. 나머지가 2인 경우 -> 포대 2개 추가 (2kg -> 12kg 포대 2개 감소 ->3kg포대 4개)
- * 	2.3. 나머지가 3인 경우 -> 포대 1개 추가 (3kg -> 3kg포대 1개)
- * 	2.3. 나머지가 4인 경우 -> 포대 2개 추가 (4kg -> 9kg 포대 1개 감소 -> 3kg 포대 3개)
+ * 문제
+ * 1. 설탕 5킬로, 3킬로 있음
+ * 2. 킬로가 주어질 때 몇 봉지인지 구하기
+ * 
+ * 풀이
+ * 1. N입력 받음 -> 4,7일때만 미리 처리
+ * 2. N을 5로 나눈 나머지
+ * 3. N을 5로 나눈 값
+ * 4. 그리디 사용 -> 0이 될때 까지 +5, 3으로 나누기
  */
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class Main {
 	public static void main(String[] args) {
-				Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		if(N==4 ||N==7 ) {
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		if(N==4 || N == 7) {
 			System.out.println(-1);
-		}else {
-			int count = N/5;
-			int temp = N%5;
-			switch(temp) {
-			case 0: break;
-			case 1: count +=1; break;
-			case 2 : count +=2; break;
-			case 3 : count +=1; break;
-			case 4 : count +=2; break;
-			}
-			System.out.println(count);
+			return;
 		}
+		long sugar = N%5;
+		long group = N/5;
+		while(true) {
+			if(sugar%3==0) {
+				group += sugar/3;
+				break;
+			}
+			sugar += 5;
+			group --;
+		}
+		System.out.println(group);
 	}
 }
