bcc2ddeacdf6a361ce65084e2f579e2e544167cc
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-02-15 21:11:11 +0900

    [Bronze I] Title: 최소공배수, Time: 104 ms, Memory: 12264 KB -BaekjoonHub

---
+++ 백준/Bronze I/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,40 @@
+/*
+ * 문제
+ * 1. 최대 공약수 최소 공배수 구하기
+ * 
+ * 풀이
+ * 1. 숫자 A,B입력
+ * 2. 최소 공약수 구하기
+ * 	2.1. 함수 사용
+ * 		2.1.1. 둘 중 큰 수를 작은 수로 나눔 -> 안나눠지면 나머지와 작은수를 다시 넣음
+ * 		2.1.2. 나눠지면 작은 수가 최소 공약수
+ * 3. 최소 공배수 구하기 -> 두수 곱 / 최소 공약수
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int t=0;t<T;t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int withmul = gcd(A,B);
+			sb.append(A*B/withmul+"\n");
+		}
+		System.out.println(sb);
+		
+	}
+	public static int gcd(int a, int b) {
+		if(b>a) {
+			int temp = a;
+			a=b;
+			b=temp;
+		}
+		if(a%b==0) return b;
+		return gcd(a%b,b);
+	}
+}
