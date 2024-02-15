1d49bcd829a1c5325774476a8cc361c724a841e3
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-02-15 21:06:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 112 ms, Memory: 12868 KB -BaekjoonHub

--- 백준/Bronze I/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze I/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -5,6 +5,9 @@
  * 풀이
  * 1. 숫자 A,B입력
  * 2. 최소 공약수 구하기
+ * 	2.1. 함수 사용
+ * 		2.1.1. 둘 중 큰 수를 작은 수로 나눔 -> 안나눠지면 나머지와 작은수를 다시 넣음
+ * 		2.1.2. 나눠지면 작은 수가 최소 공약수
  * 3. 최소 공배수 구하기 -> 두수 곱 / 최소 공약수
  */
 import java.io.*;
@@ -15,14 +18,17 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int A = sc.nextInt();
 		int B = sc.nextInt();
-		int withmul =0;
-		for(int i = Math.min(A, B);i>0;i--) {
-			if(A%i==0 && B%i==0) {
-				withmul = i;
-				break;
-			}
-		}
+		int withmul = gcd(A,B);
 		System.out.println(withmul);
 		System.out.println(A*B/withmul);
+	}
+	public static int gcd(int a, int b) {
+		if(b>a) {
+			int temp = a;
+			a=b;
+			b=temp;
+		}
+		if(a%b==0) return b;
+		return gcd(a%b,b);
 	}
 }
