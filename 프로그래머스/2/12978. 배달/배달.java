861ccac390a283e7d77f6ac5d40cf82793f3bf31
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-02-25 23:58:15 +0900

    [level 2] Title: 배달, Time: 2600.32 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,63 +1,78 @@
 
-/*
- * 문제
- * 1. 각 마을까지 가는데 경로에 값이 존재
- * 2. K의 시간이 주어짐
- * 3. 1번 마을에서 시간내에 이동이 가능한 마을 갯수 세기
- * 
- * 풀이
- * 1. N의 갯수 입력 받기
- * 2. 간선들이 주어짐
- * 3. bfs로 탐색 하며 K이하인 거리들 찾기
- * 
- * bfs
- * 1. 노드 번호와 지금까지의 거리 넣기
- * 2. {1,0} 큐에 넣기
- * 3. while큐 빌 때까지
- * 	3.1. 큐에서 꺼내기
- * 	3.2.  연결된 간선 들 확인
- * 	3.3. visited면 pass
- * 	3.4. 가중치 + 거리 >K이면 pass
- * 	3.5. 큐에 넣기 -> count++
- * 
- */
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 프로그래머스 배달
+ *
+ * 조건
+ * 마을 개수(정점) N : 1 ~ 50
+ * 도로 정보의 개수(간선) M : 1 ~ 2000
+ * 양방향 통행(무향 그래프)
+ * 입력 방식 : 정점 a 정점 b 가중치 c
+ * c : 1 ~ 10000
+ *
+ * 문제에서 구하고자 하는 것
+ * 음식 주문을 받을 수 있는 마을의 개수 (1번 마을 기준 <- 시작 정점)
+ *
+ * 문제 해결 프로세스
+ * 인접 행렬 생성
+ * 1번 탐색으로 시작해 dfs, 가중치가 k를 넘지 않는 경우만
+ *
+ * 고려한 시간 복잡도
+ * 50 * 50 = 2500
+ *
+ * */
+
 public class Solution {
-	int k;
-	boolean visited[];
-	boolean checked[];
-	List<int[]>[] listArr;
-	int answer = 1;
+    static List<int[]>[] adjList;
+    static boolean[] isChecked;
+    static int count = 1;
+ static boolean[] visited;
+
     public int solution(int N, int[][] road, int K) {
-    	listArr = new List[N+1];
-    	for(int i =1;i<=N;i++) {
-    		listArr[i] = new ArrayList<int[]>();
-    	}
-    	for(int i = 0;i<road.length;i++) {
-    		listArr[road[i][0]].add(new int[] {road[i][1],road[i][2]});
-    		listArr[road[i][1]].add(new int[] {road[i][0],road[i][2]});
-    	}
-    	visited = new boolean[N+1];
-    	checked = new boolean[N+1];
-    	visited[1] = true;
-    	checked[1] = true;
-    	k = K;
-    	
-		dfs(0,1);
-    	
+               adjList = new List[N+1];
+        for(int i = 1; i < N+1; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        isChecked = new boolean[N+1];
+        visited = new boolean[N+1];
+        A : for(int i = 0; i < road.length; i++) {
+            int a = road[i][0];
+            int b = road[i][1];
+            int c = road[i][2];
+            for(int j = 0; j < adjList[a].size(); j++) {
+                if(adjList[a].get(j)[0] == b) {
+                    if(adjList[a].get(j)[1] > c) {
+                        adjList[a].remove(j);
+                        break;
+                    }
+                    else continue A;
+                }
+            }
+            adjList[a].add(new int[]{b, c});
+            adjList[b].add(new int[]{a, c});
+        }
+        isChecked[1] = true;
+        visited[1] = true;
+        dfs(1, K);
+
+        int answer = count;
         return answer;
     }
-    public void dfs(int dis,int node) {
-    	if(dis>k) return;
-    	if(!checked[node]) {
-    		checked[node] = true;
-    		answer++;
-    	}
-    	for(int[] data : listArr[node] ) {
-			if(visited[data[0]]) continue;
-			visited[data[0]]= true;
-			dfs(dis+data[1],data[0]);
-			visited[data[0]]= false;
-			}
+
+    private static void dfs(int current, int K) {
+        for(int[] arr : adjList[current]) {
+        	if(visited[arr[0]]) continue;
+            if(arr[1] > K) continue;
+            if(!isChecked[arr[0]]) count++;
+            isChecked[arr[0]] = true;
+            visited[arr[0]] = true;
+            dfs(arr[0], K- arr[1]);
+            visited[arr[0]] = false;
+        }
     }
 }
