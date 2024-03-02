466847ff720707b05dd16c3ba57c8233bd00fb79
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-03-02 22:23:03 +0900

    [Gold V] Title: 탑, Time: 688 ms, Memory: 163676 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,40 +1,41 @@
 /*
  * 문제
- * 1. 탑의 높이 주어짐
- * 2. 오른쪽에서 왼쪽으로 레이저 발사
- * 3. 높이가 레이저가 쏜 탑이 더 높으면 위쪽으로 레이저가 셈
- * 4. 가장 꼭대기의 레이저를 받는 탑을 구하기
+ * 1. 탑들이 존재
+ * 2. 가장 꼭대기에서 레이저 발사
+ * 3. 레이저가 닿은 탑의 위치 출력 -> 아무 탑에도 도착을 안하면 0 출력
  * 
- *  풀이
- *  1. 탑의 갯수 입력 N -> 	500000
- *  2. stack만들기 -> 가장 아래 index 0, height MAX_VALUE넣기
- *  3. 탑의 높이 입력 받으면서 index와 함께 stack에 넣기
- *  4. peek을 통해서 확인을 하면서 현제 탑의 높이가 더 크면 다 뽑아 버리면서 제거
- *  5. 마지막에 박은 탑의 index 출력
+ * 풀이
+ * 1. N입력
+ * 2. stack생성
+ * 3. stack에 {MAX값,0}넣기
+ * 4. for(1~N돌리기)
+ * 	4.1. stack.peek
+ * 		4.1.1. 현재 값과 비교해서 peek한 값이 큰 경우 => peek한 뒤의 값 출력
+ * 		4.1.2. peek한 값이 더 작으면 제거 후 다음 값 peek    
  */
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Stack<int[]> stack = new Stack<int[]>();
+		Deque<int[]> stack = new ArrayDeque<>();
 		stack.push(new int[] {Integer.MAX_VALUE,0});
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 1;i<=N;i++) {
-			int temp = Integer.parseInt(st.nextToken());
+			int nowH = Integer.parseInt(st.nextToken()); 
 			while(true) {
-				if(stack.peek()[0]<temp) {
-					stack.pop();
-				}else {
-					sb.append(stack.peek()[1] + " ");
-					stack.push(new int[] {temp,i});
+				int[] temp = stack.peek();
+				if(temp[0]>=nowH) {
+					sb.append(temp[1]+" ");
+					stack.push(new int[] {nowH,i});
 					break;
+				}else {
+					stack.pop();
 				}
 			}
-			
 		}
 		System.out.println(sb);
 	}
