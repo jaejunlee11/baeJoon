067d69ce11630bae805b617230361f807bf40895
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-03-15 23:38:47 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 500 ms, Memory: 19464 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = 1;i<2*N;i++) {
+			for(int j = 1;j<2*N;j++) {
+				if(Math.abs(N-j)<Math.min(i, 2*N-i)) {
+					System.out.print("*");
+				}else if(j<N){
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
