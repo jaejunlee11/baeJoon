01feaab9569b58e2a5085fa469d76ce6f9778df1
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-04-28 13:47:21 +0900

    [Bronze II] Title: Hashing, Time: 80 ms, Memory: 11516 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,11 +3,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        long N = Integer.parseInt(br.readLine());
         String temp = br.readLine();
-        int answer = 0;
+        long answer = 0;
         for(int i = 0;i<N;i++){
-            answer += (int)Math.pow(31, i)*((temp.charAt(i)-'a')+1);
+            long an = 1;
+            for(int j = 0;j<i;j++){
+                an = (an * 31)% 1234567891;
+            }
+            answer += (an*((temp.charAt(i)-'a')+1)% 1234567891);
             answer = answer % 1234567891;
         }
         System.out.println(answer);
