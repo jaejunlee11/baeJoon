c6d4ed040dbf29bf8339e41c811b6c708d3d9f5c
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-05-06 23:19:25 +0900

    [Bronze II] Title: 대표값2, Time: 84 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[5];
+		int sum = 0;
+		for(int i = 0;i<5;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum+=arr[i];
+		}
+		Arrays.sort(arr);
+		System.out.println(sum/5);
+		System.out.println(arr[2]);
+	}
+}
