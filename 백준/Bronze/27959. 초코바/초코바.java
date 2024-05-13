7e1e9be44710eff51352d000750f35e141e10828
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-05-13 22:17:10 +0900

    [Bronze V] Title: 초코바, Time: 112 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a*100>=b) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+	}
+}
