9168257f2fed1838bf0cb6846150512e66c8fcad
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-05-21 00:51:05 +0900

    [Bronze V] Title: HI-ARC=?, Time: 112 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		System.out.println(a*b-c*d*e);
+	}
+}
