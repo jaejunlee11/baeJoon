91b1ed4bda7975d3196300d86ad881a7e496d3bd
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-05-25 23:28:08 +0900

    [Bronze V] Title: 나는 누구인가, Time: 76 ms, Memory: 11388 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+/*
+ * 문제
+ * 1. moo라는 글자로 시작
+ * 2. s(i)는 s(i-1) + m + o*(i+2) + s(i-1)
+ * 3. n이 주어질때 n의 글자는?
+ * 
+ * 아이디어
+ * 1./ 2, 3 , 2, 4, 2, 3, 2 ,5, 2, 3, 2, 4, 2, 
+ * 
+ * 풀이
+ * 1. n입력 => 1, 2, 3, 은 m , o , o
+ * 2. for문 돌리기 4~n
+ * 	2.1. 
+ * 	
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("I'm Sexy");
+	}
+}
