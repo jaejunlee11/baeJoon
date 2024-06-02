d51e9766c5798fa403d23cc339012a8022f53fb0
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-06-02 22:38:38 +0900

    [Bronze V] Title: 사파리월드, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		System.out.println(Math.abs(a-b));
+		
+		
+	}
+//	public static long recur(int count) {
+//		if(count==0) {
+//			return 1;
+//		}
+//		return count * recur(count-1);
+//	}
+}
