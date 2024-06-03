d4896c4197547c3940562030563dfaf3106f0f55
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-06-03 22:19:43 +0900

    [Bronze V] Title: 제리와 톰, Time: 112 ms, Memory: 12984 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println((b-a)+" "+b);
+	}
+//	public static long recur(int count) {
+//		if(count==0) {
+//			return 1;
+//		}
+//		return count * recur(count-1);
+//	}
+}
