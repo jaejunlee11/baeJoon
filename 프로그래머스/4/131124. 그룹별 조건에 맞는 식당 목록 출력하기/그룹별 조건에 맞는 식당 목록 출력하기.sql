44f5ccca2979a7b93a6938ad04aa3b0afefaa6c6
gPassionFruit <72538151+jaejunlee11@users.noreply.github.com>
2024-10-19 01:07:18 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,27 +1,17 @@
--- 코드를 입력하세요
-# 회원 ID, 회원 이름, 회원 연락처, 성별, 생년월일
-# 리뷰 ID, 식당 ID, 회원 ID, 점수, 리뷰 텍스트, 리뷰 작성일
-# 1.리뷰를 가장 많이 작성한 회원의 리뷰들을 조회
-# 2. 리뷰 작성일을 기준으로 오름차순, 리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬
-
-SELECT m.MEMBER_NAME, r.REVIEW_TEXT, Date_format(r.REVIEW_DATE,"%Y-%m-%d") as "REVIEW_DATE"
-from MEMBER_PROFILE m
-inner join REST_REVIEW r
-on m.MEMBER_ID = r.MEMBER_ID
-where m.MEMBER_ID in (SELECT m.MEMBER_ID
-from MEMBER_PROFILE m
-inner join REST_REVIEW r
-on m.MEMBER_ID = r.MEMBER_ID
-group by m.MEMBER_ID
-having count(*) >= all (SELECT count(*)
-from MEMBER_PROFILE m
-inner join REST_REVIEW r
-on m.MEMBER_ID = r.MEMBER_ID
-group by m.MEMBER_ID))
-order by `REVIEW_DATE`, REVIEW_TEXT;
-
-# (SELECT count(*)
-# from MEMBER_PROFILE m
-# inner join REST_REVIEW r
-# on m.MEMBER_ID = r.MEMBER_ID
-# group by m.MEMBER_ID);
+-- Member_ID, MEMBER_NAME, TLNO,GENDER,DATE_OF_BIRTH
+-- 회원 ID, 회원 이름, 회원 연락처, 성별, 생년월일
+-- REVIEW_ID, REST_ID, MEMBER_ID, REVIEW_SCORE, REVIEW_TEXT, REVIEW_DATE
+-- 리뷰 ID, 식당 ID, 회원 ID, 점수, 리뷰 텍스트, 리뷰 작성일
+-- 리뷰를 가장 많이 작성한 회원의 리뷰
+select A.MEMBER_NAME, B.REVIEW_TEXT, DATE_FORMAT(B.REVIEW_DATE, "%Y-%m-%d") as "REVIEW_DATE"
+from (select A.MEMBER_NAME, A.MEMBER_ID
+from MEMBER_PROFILE A
+left join (SELECT RANK() over(order by countREVIEW desc) as "review_rank", MEMBER_ID
+from (SELECT MEMBER_ID, count(*) as "countREVIEW"
+from REST_REVIEW
+group by MEMBER_ID) sub) B
+on A.MEMBER_ID = B.MEMBER_ID
+where B.review_rank = 1) A
+left join REST_REVIEW B
+on A.MEMBER_ID = B.MEMBER_ID
+order by REVIEW_DATE, REVIEW_TEXT;
